1de878cbf24d7eb5550f351856703f4a6649a01b
beeMonkey <675383749@qq.com>
2018-07-08 16:18:22 +0800

    add calsubtotal function and build a single test fot it

--- main/best-charge.js
+++ main/best-charge.js
@@ -3,7 +3,8 @@ let selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
 function bestCharge(selectedItems) {
   const ItemsCount = calItemsCount(selectedItems)
   const cartItems = buildcartItems(ItemsCount, loadAllItems())
-  console.log(cartItems)
+  const cartItemsWith_subtotal = calsubtotal(cartItems)
+  console.log(cartItemsWith_subtotal)
 }
 
 function calItemsCount(selectedItems) {
@@ -11,22 +12,31 @@ function calItemsCount(selectedItems) {
     let [id, count] = selectedItem.split(' x ')
     return {
       id,
-      count
+      count: parseInt(count)
     }
   })
 }
 function buildcartItems(ItemsCount, loadAllItems) {   //在进行map操作的时候，先遍历小的数组，在遍历大的，不然会多出一些map
   return ItemsCount.map((itemcount) => {
     let { id, count } = itemcount;
-    const sameid= loadAllItems.find((itemInAll) => {
+    const sameid = loadAllItems.find((itemInAll) => {
       return itemcount.id === itemInAll.id;
     })
-    let { name, price } =sameid;
+    let { name, price } = sameid;
     return { id, name, price, count }
   })
 }
-bestCharge(selectedItems)
+function calsubtotal(cartItems) {
+  return cartItems.map((cartItem) => {
+    const subtotal = cartItem.price * cartItem.count
+    cartItem.subtotal=subtotal
+    return cartItem
+  })
+}
+
+//bestCharge(selectedItems)
 module.exports = {
   calItemsCount,
-  buildcartItems
+  buildcartItems,
+  calsubtotal
 }
--- spec/calItemsCount-spec.js
+++ spec/calItemsCount-spec.js
@@ -1,12 +1,12 @@
 const main = require('../main/best-charge')
-describe('Take out food', function () {
+describe('Test for calItemsCount', function () {
 
-    it('should return the [{barcode,count}]', function() {
+    it('should return the [{id,count}]', function() {
       let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
       let outputs = main.calItemsCount(inputs);
-      let expected =[ { id: 'ITEM0001', count: '1' },
-      { id: 'ITEM0013', count: '2' },
-      { id: 'ITEM0022', count: '1' } ] 
+      let expected =[ { id: 'ITEM0001', count: 1 },
+      { id: 'ITEM0013', count: 2 },
+      { id: 'ITEM0022', count: 1 } ] 
       expect(outputs).toEqual(expected)
     });
   });
