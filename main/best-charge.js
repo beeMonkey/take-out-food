4d6d837610b2ab101c84abddf00343605741dec4
beeMonkey <675383749@qq.com>
2018-07-08 15:50:25 +0800

    add buildcartItems function and build a single test for it

--- main/best-charge.js
+++ main/best-charge.js
@@ -1,17 +1,32 @@
+const { loadAllItems } = require('./items.js')
+let selectedItems = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
 function bestCharge(selectedItems) {
-
+  const ItemsCount = calItemsCount(selectedItems)
+  const cartItems = buildcartItems(ItemsCount, loadAllItems())
+  console.log(cartItems)
 }
 
-function  calItemsCount(selectedItems){
-    return selectedItems.map((selectedItem)=>{
-        let [barcode,count] =  selectedItem.split('x')
-        return {
-          barcode,
-          count
-        }
+function calItemsCount(selectedItems) {
+  return selectedItems.map((selectedItem) => {
+    let [id, count] = selectedItem.split(' x ')
+    return {
+      id,
+      count
+    }
   })
-} 
-
-module.exports={
-  calItemsCount
+}
+function buildcartItems(ItemsCount, loadAllItems) {   //在进行map操作的时候，先遍历小的数组，在遍历大的，不然会多出一些map
+  return ItemsCount.map((itemcount) => {
+    let { id, count } = itemcount;
+    const sameid= loadAllItems.find((itemInAll) => {
+      return itemcount.id === itemInAll.id;
+    })
+    let { name, price } =sameid;
+    return { id, name, price, count }
+  })
+}
+bestCharge(selectedItems)
+module.exports = {
+  calItemsCount,
+  buildcartItems
 }
---
+++ spec/buildcartItems-spec.js
@@ -0,0 +1,15 @@
+const main = require('../main/best-charge')
+const {calItemsCount,buildcartItems}=require('../main/best-charge')
+const { loadAllItems } = require('../main/items.js')
+describe('Take out food', function () {
+
+    it('should return the [{barcode,count}]', function() {
+      let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+      let itemsCount = calItemsCount(inputs);
+      let cartItems=buildcartItems(itemsCount,loadAllItems())
+      let expected =[{ id: 'ITEM0001', name: '黄焖鸡', price: 18, count: '1' },
+      { id: 'ITEM0013', name: '肉夹馍', price: 6, count: '2' },
+      { id: 'ITEM0022', name: '凉皮', price: 8, count: '1' } ]
+      expect(cartItems).toEqual(expected)
+    });
+  });
